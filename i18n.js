3be972aae3f9b21c86d07b5242db38eaceac15d3
Julien Tilap LA VINH <jlavinh@gmail.com>
2021-02-01 01:51:18 +0100

    fix: load containers translations

--- i18n.js
+++ i18n.js
@@ -7,8 +7,16 @@ module.exports = {
     '*': ['common', 'site'],
     '/': ['page-home'],
     '/about': ['page-about'],
-    '/links': ['page-links'],
+    '/links': ['page-links', 'container-LinksList'],
   },
   logBuild: process.env.NODE_ENV === 'development',
-  loadLocaleFrom: (lang, ns) => import(`./${localesFolder}/${lang}/${ns}.json`).then((m) => m.default),
+  loadLocaleFrom: (lang, ns) => {
+    // Containers
+    if (ns.startsWith('container-')) {
+      const [_, containerName] = ns.split('-');
+      return import(`./src/containers/${containerName}/${localesFolder}/${lang}.json`).then((m) => m.default);
+    }
+    // Globals
+    return import(`./${localesFolder}/${lang}/${ns}.json`).then((m) => m.default);
+  },
 };
