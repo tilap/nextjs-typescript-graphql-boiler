e82b2eb6523df3df306003818b2c272ef5abd792
Julien Tilap LA VINH <jlavinh@gmail.com>
2021-02-02 01:16:57 +0100

    feat: add 404 custom error page

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next';
+
+const NotFoundPage: NextPage = () => {
+  return (
+    <div>
+      <p>Sorry, page not found</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
